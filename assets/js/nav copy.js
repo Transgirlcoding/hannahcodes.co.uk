95bf80346f2b2b4fdd35026a83a7cf1bba33a028
Hannah <hannah@hannahcodes.co.uk>
2022-04-12 00:41:34 +0100

    test

--- assets/js/nav copy.js
+++ assets/js/nav copy.js
@@ -1,6 +1,6 @@
 const hamburger = document.querySelector(".blog_hamburger");
 const navLinks = document.querySelector(".blog_nav-links");
-const links = document.querySelectorAll(".blog_nav-links blog_nav_li");
+const links = document.querySelectorAll(".blog_nav-links .blog_nav_li");
 
 hamburger.addEventListener('click', ()=>{
    //Animate Links
